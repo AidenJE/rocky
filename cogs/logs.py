0445d69b3b2019d17901b12592c60f83c2c6c686
AidenJE <majoraaj@gmail.com>
2022-01-09 14:51:28 -0500

    stripped some stuff

--- cogs/logs.py
+++ cogs/logs.py
@@ -3,37 +3,8 @@ from utils import crud
 
 
 class Logs(commands.Cog):
-    """
-    Register and track users.
-
-    This cog serves as a baseline for all games and 
-    utilities that require some form of data storage.
-    """
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        for guild in self.bot.guilds:
-            crud.register_guild(guild)
-
-            for member in guild.members:
-                if await crud.is_member_registered(member):
-                    crud.register_guildmember(member)
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        if await crud.is_member_registered(member):
-            crud.register_guildmember(member)
-
-    @commands.Cog.listener()
-    async def on_guild_join(self, guild):
-        crud.register_guild(guild)
-
-    @commands.command()
-    async def register(self, ctx):
-        await crud.register_member(ctx.author) 
-        await ctx.reply("Your account was succesfully created!")
 
 
 def setup(bot):
--- utils/models.py
+++ utils/models.py
@@ -3,21 +3,7 @@ from gino import Gino
 db = Gino()
 
 
-class Guild(db.Model):
-   __tablename__ = "guilds" 
-
-   id = db.Column(db.BigInteger(), primary_key=True)
-
-
 class Member(db.Model):
     __tablename__ = "members"
 
     id = db.Column(db.BigInteger(), primary_key=True)
-
-
-class GuildMember(db.Model):
-    __tablename__ = "guildmembers"
-
-    id = db.Column(db.BigInteger(), primary_key=True, autoincrement=True)
-    guild_id = db.Column(db.BigInteger(), db.ForeignKey("members.id"), nullable=False)
-    member_id = db.Column(db.BigInteger(), db.ForeignKey("guilds.id"), nullable=False)
